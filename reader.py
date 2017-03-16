0aac2eb25e12af97bca9c9580daddccbadd1ae9b
alyssadmoore <alyssadmoore@gmail.com>
2017-03-16 17:03:30 -0500

    Fix typo in reader.py

--- reader.py
+++ reader.py
@@ -4,8 +4,10 @@ import re
 def get_dates():
     dates = []
 
-    with open("quarter-info.txt", encoding="utf8") as f:
+    with open("quarter_info.txt", encoding="utf8") as f:
         for line in f:
             if "Coin Release: " in line:
                 date = re.findall("Release: (.*) Release", line, re.DOTALL)
                 dates.append(date)
+
+    return dates
