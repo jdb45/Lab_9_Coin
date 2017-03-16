4c3da3075ded0eb4dbc1c970d423e340be2e301f
alyssadmoore <alyssadmoore@gmail.com>
2017-03-16 15:57:42 -0500

    Add get_dates method

--- reader.py
+++ reader.py
@@ -1,10 +1,11 @@
 import re
 
-with open("page.txt", encoding="utf8") as f:
-    for line in f:
-        if "Coin Release: " in line:
-            print(line)
 
-test_string = "Statehood: December 7, 1787 Coin Release: January 4, 1999 Release Order: 1 Total Mintage for Circulation: 774,824,000"
-testRE = re.findall("Release: (.*)Release", test_string, re.DOTALL)
-print(testRE)
+def get_dates():
+    dates = []
+
+    with open("page.txt", encoding="utf8") as f:
+        for line in f:
+            if "Coin Release: " in line:
+                date = re.findall("Release: (.*) Release", line, re.DOTALL)
+                dates.append(date)
