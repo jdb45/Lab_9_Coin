d4b56bdea4361dc86b7840ab95dbda92ac96ddd0
alyssadmoore <alyssadmoore@gmail.com>
2017-03-16 15:58:54 -0500

    Correct text file name

--- reader.py
+++ reader.py
@@ -4,7 +4,7 @@ import re
 def get_dates():
     dates = []
 
-    with open("page.txt", encoding="utf8") as f:
+    with open("quarter-info.txt", encoding="utf8") as f:
         for line in f:
             if "Coin Release: " in line:
                 date = re.findall("Release: (.*) Release", line, re.DOTALL)
