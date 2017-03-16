7fd7a0fccc81232ffff79945c8840cedcaee5466
Brendon <brennon.mckeever@gmail.com>
2017-03-16 16:34:52 -0500

    still working on data stripping

--- quarter_info_processing.py
+++ quarter_info_processing.py
@@ -1,11 +1,27 @@
 import re
 
 quarter_data = []
+design_data = []
 
+# This section works, and will return the dates the coin was released into circulation.
 with open('quarter_info.txt', 'r') as info:
 
     for line in info:
 
         if 'Coin Release' in line:
 
-            print(line)
+            testRE = re.findall("Release: (.*) Release", line, re.DOTALL)
+            quarter_data.append(testRE)
+
+# This code will eventually add the Design description . TODO this is not working.
+with open('quarter_info.txt', 'r') as info:
+
+    for line in info:
+
+        if 'Design' in line:
+
+            testRE = re.findall("Design: (.*) ", line, re.DOTALL)
+
+            design_data.append(testRE)
+
+print(design_data)
