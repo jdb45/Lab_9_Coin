4fb91d01c179d541313ea9f206497aa92d59eb41
alyssadmoore <alyssadmoore@gmail.com>
2017-03-19 16:14:46 -0500

    Add get_states method

--- quarter_info_processing.py
+++ quarter_info_processing.py
@@ -1,6 +1,27 @@
 import re
 
 
+# Returns state names in the order in the document
+# todo hacky way for now, edit if you find a better method
+def get_states():
+    states = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware",
+              "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana",
+              "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana",
+              "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina",
+              "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina",
+              "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia",
+              "Wisconsin", "Wyoming"]
+    states_reordered = []
+    with open('quarter_info.txt', 'r', encoding='utf8') as f:
+        for line in f:
+            if line.title().strip() in states:
+                states_reordered.append(line.title().strip())
+
+    return states_reordered
+
+print(get_states())
+
+
 # Returns the dates each coin was released into circulation
 def get_dates():
     dates = []
@@ -24,5 +45,3 @@ def get_descriptions():
                 descriptions.append(description[0])
 
     return descriptions
-
-print(get_descriptions())
